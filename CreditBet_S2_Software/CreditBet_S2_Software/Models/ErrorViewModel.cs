2cf8ceeab8b0ef09187d2e1e1cee3db3d5dee282
Randy Barents <33863457+koekenpan100@users.noreply.github.com>
2020-10-07 13:48:29 +0200

    Creation-Project-And-Added-DatabaseLogic
    creation of project , adding of dapper and added database logic

---
+++ CreditBet_S2_Software/CreditBet_S2_Software/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CreditBet_S2_Software.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CreditBet_S2_Software/DataLayer/DataAccess/DatabaseAccess.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
+namespace DataLayer.DataAccess
+{
+    public class DatabaseAccess
+    {
+        public static string GetConnectionString()
+        {
+            return "";
+        }
+
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection con = new MySqlConnection(GetConnectionString()))
+            {
+                return con.Query<T>(sql).ToList();
+            }
+        }
+
+        public static T LoadFirstData<T>(string sql)
+        {
+            using (IDbConnection con = new MySqlConnection(GetConnectionString()))
+            {
+                try
+                {
+                    return con.Query<T>(sql).FirstOrDefault();
+                }
+                catch (InvalidCastException e)
+                {
+                    return default;
+                }
+            }
+        }
+
+        public static void SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection con = new MySqlConnection(GetConnectionString()))
+            {
+                con.Execute(sql, data);
+            }
+        }
+
+        public static void DeleteData(string sql)
+        {
+            using (IDbConnection con = new MySqlConnection(GetConnectionString()))
+            {
+                con.Execute(sql);
+            }
+        }
+    }
+}
