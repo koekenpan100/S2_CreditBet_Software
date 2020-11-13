eb25fdb0dcf0bdf50b5452e93e44a0f086e24a76
Randy Barents <33863457+koekenpan100@users.noreply.github.com>
2020-11-13 10:26:31 +0100

    Creation item datamodel and processor

---
+++ CreditBet_S2_Software/DataLayer/DataLogic/ItemProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer.DataModels;
+using DataLayer.DataAccess;
+
+namespace DataLayer.DataLogic
+{
+    public class ItemProcessor
+    {
+        public static void CreateItem(string name, string description, decimal price, string category)
+        {
+            ItemDataModel data = new ItemDataModel
+            {
+                Name = name,
+                Description = description,
+                Price = price,
+                Category = category
+            };
+            string sql = @"INSERT INTO user (name, description, price, category)
+                            VALUES(@Name, @Description, @Price, @Category);";
+            DatabaseAccess.SaveData(sql , data);
+        }
+
+        public static List<ItemDataModel> LoadItems()
+        {
+            string sql = "SELECT * FROM item";
+            return DatabaseAccess.LoadData<ItemDataModel>(sql);
+        }
+        
+        public static void DeleteItem(int id)
+        {
+            string sql = $"DELETE FROM item WHERE id = '{id}';";
+            DatabaseAccess.DeleteData(sql);
+        }
+    }
+}
